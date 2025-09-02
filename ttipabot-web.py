27987dc07cded59611fd472206174db417b5339d
Louis Habberfield-Short <louishabberfieldshort@gmail.com>
2025-09-03 01:06:07 +1200

    Linting

--- ttipabot-web.py
+++ ttipabot-web.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 
@@ -8,6 +6,7 @@ from app.models import Attorney, Firm
 
 app = create_app()
 
+
 @app.shell_context_processor
 def make_shell_context():
-    return {'sa': sa, 'so': so, 'db': db, 'User': Attorney, 'Firm': Firm}
+    return {"sa": sa, "so": so, "db": db, "User": Attorney, "Firm": Firm}
